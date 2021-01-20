c5597164c878711ce7ccf12275dd34c3d067d93e
lida-3 <lidija.djikic@yahoo.com>
2021-01-20 12:32:26 +0100

    adding 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,25 @@
 import React from "react"
+import styles from '../layouts/notfound.module.css'
+import {navigate} from 'gatsby'
 
-const NotFoundPage = () => (
-  <div>
-    <h1>ayy... nothing here!</h1>
-    <p>You just hit a route that doesn&#39;t exist...
-    the sadness.</p>
+const NotFoundPage = () => {
+return (
+  <div className={styles.container}>
+    <div className={styles.txtdiv}>
+    <h1>Upss.. stranica nije pronađena</h1>
+    <p>Tražena stranica ne postoji, možete se vratiti na naslovicu.</p>
+    </div>
+    <div className={styles.buttondiv}>
+      <button onClick={()=>navigate('/naslovnica')}>Povratak na naslovicu</button>
+    </div>
+    <div className={styles.imgdiv}>
+      <img className={styles.img} src={require('../images/404error.jpg')}></img>
+    </div>
+    
   </div>
 )
-
+}
 export default NotFoundPage
+
+
+
